c6ecef40408b35137b29f04ac26ad2bcb54a7847
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-06-29 14:49:43 +0100

    Desktop styling and active state

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         </footer>
       </div>
       <footer>
-        <p class="attribution">
+        <p className="attribution">
           Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
           Coded by <a href="#">Your Name Here</a>.
         </p>
